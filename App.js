6bb195d0833583ccb483d637982ecb2f34e3cf4a
choompol-edipaorg <choompol@edipa.org>
2020-04-23 10:56:13 +0700

    f1

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 import React, { useReducer, useState, useEffect } from 'react';
 import { Button, Text, TextInput, View, TouchableOpacity, 
   StyleSheet, AsyncStorage } from 'react-native';
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, useFocusEffect } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
@@ -49,6 +49,10 @@ const getUser = async () => {
 const InputScreen = ({navigation}) => {
   const [state,dispatch] = useReducer(reducer, getUser());
   const {id,name,temp} = state;
+  const sendTemp = async () => {
+    let us = await getUser();
+    console.log("SEND TEMP TO SERVER ID:"+ us.id+" TEMP:"+ us.temp);
+  };
   return (
     <View style={styles.input}>
       <Text>===</Text>
@@ -61,6 +65,7 @@ const InputScreen = ({navigation}) => {
       <Text>Body Temperature</Text>
       <TextInput style={styles.text} autoCapitalize="none" autoCorrect={false}
         value={temp} onChangeText={ val => dispatch({type:'temp', payload:val})} />
+      <Button onPress={()=>sendTemp()} title="SEND TEMP"/>
     </View>
   );
 }
@@ -74,17 +79,20 @@ const QRScanScreen = ({ navigation}) => {
       setHasPermission(status === 'granted');
     })();
   }, []);
+  useEffect(() => { setScanned(false); });
+
   const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
     let us = await getUser();
-    fetch('https://www.google.com', {
-      method: 'GET',
+    let url = "https://ssjnonthaburi.moph.go.th/covidscan/index.php?module=consumer_scan&a851eb48ddeae74f465ae5719bba6933&fmod=token&SERVANTID=1330500022874&PHONEID=0879581794";
+    fetch(url, { method: 'GET', }) .then((resp) => { return resp.text(); })
+    .then((text) => {
+      console.log("DATA OK: ");
+      navigation.navigate('Input')
     })
-    .then((res) => console.log("GOT DATA "))
-    .catch((err) => console.log(err));
-
-    setScanned(false);
-    navigation.navigate('Input')
+    .catch((err) => {
+      setScanned(false);
+    });
   };
   if (hasPermission === null) { return <Text>ขอใช้กล้องเพื่อสแกนคิวอาร์โค้ด</Text>; }
   if (hasPermission === false) { return <Text>ไม่สามารถใช้กล้องเพื่อสแกนคิวอาร์โค้ด</Text>; }
